307a7bc828140b8f95e6867f2f1789048fc36870
Erin Paget <erin.paget@clio.com>
2025-05-18 13:45:57 -0400

    Initial creation with Bundler + rb_sys

---
+++ lib/levenshtein_rust.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "levenshtein_rust/version"
+require_relative "levenshtein_rust/levenshtein_rust"
+
+module LevenshteinRust
+  class Error < StandardError; end
+  # Your code goes here...
+end
