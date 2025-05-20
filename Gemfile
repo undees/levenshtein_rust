1ca12cc1d240a96978e3077107b200951d92d156
Erin Paget <erin.dees@hey.com>
2025-05-20 00:08:25 -0400

    Initial creation with Bundler + rb_sys

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in levenshtein_rust.gemspec
+gemspec
+
+gem "irb"
+gem "rake", "~> 13.0"
+
+gem "rake-compiler"
+
+gem "rspec", "~> 3.0"
---
+++ spec/levenshtein_rust_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LevenshteinRust do
+  it "has a version number" do
+    expect(LevenshteinRust::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
